f15ca41af537dd930a85c92777d7d1c820f62fbe
OthmanTabeche <otabeche23@gmail.com>
2026-04-07 19:13:13 +0200

    Add tests for image and link splitting

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,13 @@
 import unittest
 
-from TextNode import TextNode, TextType, split_nodes_delimiter, text_node_to_html_node
+from TextNode import (
+    TextNode,
+    TextType,
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+    text_node_to_html_node,
+)
 
 
 class TestTextNode(unittest.TestCase):
@@ -58,6 +65,47 @@ class TestTextNode(unittest.TestCase):
         new_nodes = split_nodes_delimiter(old_nodes, ",", TextType.TEXT)
         expected_nodes = [TextNode("Hello world!", TextType.TEXT)]
         self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_images_no_images(self):
+        node = TextNode("This is text with no images", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(
+            [TextNode("This is text with no images", TextType.TEXT)], new_nodes
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://www.google.com) and another [second link](https://www.apple.com)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://www.google.com"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second link", TextType.LINK, "https://www.apple.com"),
+            ],
+            new_nodes,
+        )
 
 
 if __name__ == "__main__":
